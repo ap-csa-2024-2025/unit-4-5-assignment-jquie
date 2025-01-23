df7c47c96f16cc77e084b9f3b015cb5c5228d81a
jquie <jacquelynphan03@gmail.com>
2025-01-23 18:27:26 +0000

    not done

--- Main.java
+++ Main.java
@@ -4,17 +4,33 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    printNTimes("word", 7);
+    System.out.pritnln();
+    printNums();
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int curr_index = 0; curr_index < word.length(); curr_index++)
+    {
+      for (int curr_print = 0; curr_print < N; curr_print++)
+      {
+        String letter = word.substring(curr_index, curr_index+1);
+        System.out.print(letter);
+      }
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+   for (int i = 10; i > 0; i--)
+   {
+    for (int j = 0; j < i; j++)
+    {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+   }
   }
 
   public static void uprightNumberTriangle(int N)
